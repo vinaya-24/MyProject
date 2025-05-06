bc67ded70244b854d209f0565fa7b5063a8ba3df
Vinaya <mcvinayashree@gmail.com>
2025-05-06 15:23:46 +0530

    Code updated2

--- src/test/java/stepDefinations/LoginSteps.java
+++ src/test/java/stepDefinations/LoginSteps.java
@@ -3,32 +3,28 @@ package stepDefinations;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class LoginSteps {
-
     WebDriver driver;
 
     @Given("User launches the browser")
-    public void user_launches_the_browser() {
-        driver = new ChromeDriver();  // make sure WebDriverManager is setup if needed
+    public void launchBrowser() {
+        driver = new ChromeDriver();
     }
 
     @When("User opens Google homepage")
-    public void user_opens_google_homepage() {
+    public void openGoogle() {
         driver.get("https://www.google.com");
     }
 
     @Then("Page title should contain {string}")
-    public void page_title_should_contain(String expectedTitle) {
-        String actualTitle = driver.getTitle();
-        assertTrue(actualTitle.contains(expectedTitle), "Title does not match!");
+    public void checkTitle(String expectedTitle) {
+        assertTrue(driver.getTitle().contains(expectedTitle));
     }
 
     @And("Close the browser")
-    public void close_the_browser() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void closeBrowser() {
+        driver.quit();
     }
 }
